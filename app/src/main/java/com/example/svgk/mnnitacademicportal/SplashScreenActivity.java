0031732da9bf8fee368f11e14bf724652c755170
Gopi <surapanenigopi44@gmail.com>
2018-09-02 14:09:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.svgk.mnnitacademicportal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 1000 ;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+
+    }
+}
