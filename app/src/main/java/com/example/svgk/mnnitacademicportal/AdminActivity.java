e322569e9c9f390c3d6eedc2976a28e225065e5f
Gopi <surapanenigopi44@gmail.com>
2018-09-23 14:16:57 +0530

    Initial commit

--- app/src/main/java/com/example/svgk/mnnitacademicportal/AdminActivity.java
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/AdminActivity.java
@@ -1,5 +1,6 @@
 package com.example.svgk.mnnitacademicportal;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -22,21 +23,27 @@ public class AdminActivity extends AppCompatActivity {
 
         usersList = new ArrayList<>();
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String url = "http://10.0.2.2/mnnit_database/admin_user.php";
 
-        usersList.add(new AdminUser("20174063","surapaneni venkata Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","surapaneni venkata Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","surapaneni venkata Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
-        usersList.add(new AdminUser("20174063","Gpi Krishna","COmputer Engi","SurapaneniGopi 44"));
+        //gets users data from the background
+        getUsersBackground background = new getUsersBackground();
+        background.execute(url);
+
         adapter = new AdminListAdapter(this,usersList);
         recyclerView.setAdapter(adapter);
     }
+
+    class getUsersBackground extends AsyncTask<String,Void,Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String url = params[0];
+            usersList = QueryUtils.fetchEarthquakeData(url);
+            return null;
+        }
+    }
+
 }
