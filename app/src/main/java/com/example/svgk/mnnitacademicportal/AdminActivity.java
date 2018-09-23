a0a99657cd3f8dffa697964f747b1d63910a3bf7
geekymanas <gundapunenimanas@gmail.com>
2018-09-23 19:47:52 +0530

    1

--- app/src/main/java/com/example/svgk/mnnitacademicportal/AdminActivity.java
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/AdminActivity.java
@@ -1,10 +1,12 @@
 package com.example.svgk.mnnitacademicportal;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.Button;
 
@@ -42,6 +44,47 @@ public class AdminActivity extends AppCompatActivity implements BackgroundTask.B
                 backgroundTask.execute("set_approve", usersList.get(0).REGD_NO);
             }
         });
+
+        SwipeHelper swipeHelper = new SwipeHelper(this, recyclerView) {
+            @Override
+            public void instantiateUnderlayButton(RecyclerView.ViewHolder viewHolder, List<UnderlayButton> underlayButtons) {
+                underlayButtons.add(new SwipeHelper.UnderlayButton(
+                        "Delete",
+                        0,
+                        Color.parseColor("#FF3C30"),
+                        new SwipeHelper.UnderlayButtonClickListener() {
+                            @Override
+                            public void onClick(int pos) {
+                                // TODO: onDelete
+                            }
+                        }
+                ));
+
+                underlayButtons.add(new SwipeHelper.UnderlayButton(
+                        "Transfer",
+                        0,
+                        Color.parseColor("#FF9502"),
+                        new SwipeHelper.UnderlayButtonClickListener() {
+                            @Override
+                            public void onClick(int pos) {
+                                // TODO: OnTransfer
+                            }
+                        }
+                ));
+                underlayButtons.add(new SwipeHelper.UnderlayButton(
+                        "Unshare",
+                        0,
+                        Color.parseColor("#C7C7CB"),
+                        new SwipeHelper.UnderlayButtonClickListener() {
+                            @Override
+                            public void onClick(int pos) {
+                                // TODO: OnUnshare
+                            }
+                        }
+                ));
+            }
+        };
+
 
     }
 
