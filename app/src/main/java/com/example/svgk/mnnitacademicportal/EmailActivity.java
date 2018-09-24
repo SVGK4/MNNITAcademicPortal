405c7b718dc6d8e6cdf5ee087c8dcd6d2023a983
geekymanas <gundapunenimanas@gmail.com>
2018-09-24 20:02:22 +0530

    Added Email

--- app/src/main/java/com/example/svgk/mnnitacademicportal/EmailActivity.java
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/EmailActivity.java
@@ -2,14 +2,33 @@ package com.example.svgk.mnnitacademicportal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class  EmailActivity extends Activity {
+public class EmailActivity extends Activity {
+
+    EditText editTextEmail, editTextSubject, editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
 
+        editTextEmail = findViewById(R.id.email);
+        editTextSubject = findViewById(R.id.subject);
+        editTextMessage = findViewById(R.id.multiText);
+
+        Button buttonSend = findViewById(R.id.emailBtn);
+
+        buttonSend.setOnClickListener(view -> sendEmail());
+    }
+
+    private void sendEmail() {
+        String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+        SendMail sm = new SendMail(this, email, subject, message);
+        sm.execute();
 
     }
 }
