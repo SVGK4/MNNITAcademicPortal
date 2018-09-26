0be198fe08d5c0078172abf092315f652c4250d2
geekymanas <gundapunenimanas@gmail.com>
2018-09-26 23:18:43 +0530

    Notification

--- app/src/main/java/com/example/svgk/mnnitacademicportal/LoginActivity.java
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/LoginActivity.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +30,8 @@ public class LoginActivity extends AppCompatActivity implements BackgroundTask.B
     private EditText username, userpass;
     private String user_name,user_pass;
     private ProgressDialog progressDialog;
+    private Switch adminToggle;
+    private TextInputLayout userHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,8 @@ public class LoginActivity extends AppCompatActivity implements BackgroundTask.B
         username = findViewById(R.id.user_name);
         userpass = findViewById(R.id.user_pass);
         registerBtn = findViewById(R.id.registerBtn);
+        adminToggle = findViewById(R.id.switch2);
+        userHint = findViewById(R.id.userTextInputLayout);
 
         //When Forgot Password is clicked
         forgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -97,6 +105,20 @@ public class LoginActivity extends AppCompatActivity implements BackgroundTask.B
                 startActivity(registerIntent);
             }
         });
+
+        adminToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    username.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                    userHint.setHint("Admin Username");
+                }else {
+                    username.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    userHint.setHint("Registration Number");
+                }
+            }
+        });
+
     }
 
     private void confirmLogin(){
