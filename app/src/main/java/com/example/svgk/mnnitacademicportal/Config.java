66638165dd2d84de3a31c57ff1fbda3d8d603018
Manas Abhilash <28773747+geekymanas@users.noreply.github.com>
2018-09-29 18:15:55 +0530

    Update Config.java

--- app/src/main/java/com/example/svgk/mnnitacademicportal/Config.java
+++ app/src/main/java/com/example/svgk/mnnitacademicportal/Config.java
@@ -1,6 +1,6 @@
 package com.example.svgk.mnnitacademicportal;
 
 public class Config {
-    protected static final String EMAIL ="mnnitacademic@gmail.com";
-    protected static final String PASSWORD ="mnnitrocks";
+    protected static final String EMAIL ="Place Email Here";
+    protected static final String PASSWORD ="Place Password Here";
 }
